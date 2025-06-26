dffa0a8d85078fbb7ac9ca9673bbedf9b5ac1fd9
Michael Carreno <michaelcarreno@Michaels-MacBook-Pro-10.local>
2025-06-26 11:53:18 -0500

    starting volunteer history page

---
+++ src/components/VolunteerHistory.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/VolunteerHistory.css'
+
+const VolunteerHistory = () => {
+    const VolunteerRecords = [
+        {/*sample data*/},
+        {/*sample data*/},
+        {/*sample data*/},
+    ];
+
+    return (
+        <div className="volunteer-history"></div>
+    );
+};
+
+export default VolunteerHistory;
