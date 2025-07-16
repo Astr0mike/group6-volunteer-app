82170ffb6e3e53f3a27aeba7d4734d40d5799e11
AdrianG0102 <adrian0102010202@gmail.com>
2025-07-16 11:55:26 -0500

    Update events.js

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createEvent } = require("../controllers/eventsController");
+const { createEvent } = require('../controllers/eventsController');
 
 router.post("/", createEvent);
 
