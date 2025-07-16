0eaa77d0b8ab2e5f8fc4e2c4b857ee8e8a9da377
AdrianG0102 <adrian0102010202@gmail.com>
2025-07-16 11:41:02 -0500

    Update and rename Events.js to events.js

---
+++ server/routes/events.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { createEvent } = require("../controllers/eventsController");
+
+// POST /api/events
+router.post("/", createEvent);
+
+module.exports = router;
+
