6b3545bf9126429a4e2f4a645d49581d13021a20
Neha Hooda <nehahooda124@gmail.com>
2025-07-18 19:32:37 -0500

    Updated App.test.js and package files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders navigation links for unauthenticated user', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Look for a Register or Login link
+  expect(screen.getByText(/Register/i)).toBeInTheDocument();
+  expect(screen.getByText(/Login/i)).toBeInTheDocument();
 });
+
