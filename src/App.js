dd8965c76765930408be27071da78db0e89a3e3c
Adrian Guerra <adrian0102010202@gmail.com>
2025-08-06 21:53:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // src/App.js
-// src/App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Notifications from "./components/Notifications";
